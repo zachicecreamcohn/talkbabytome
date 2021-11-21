d33ac663765ee20c04982e4ab30e58ed33e1afc0
zachicecreamcohn <85762393+zachicecreamcohn@users.noreply.github.com>
2021-11-21 14:58:12 -0600

    changed baby ipsum button so that, when on the baby ipsum page, it reads, 'Go Back', instead and routes to home page. Also added a div in between both header elements so that the linking only occurs when on top of either the i icon or baby ipsum link

--- run.py
+++ run.py
@@ -37,7 +37,7 @@ def babyipsum():
     ipsum = """Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Dictumst vestibulum rhoncus est pellentesque. Tempor id eu nisl nunc mi ipsum faucibus vitae aliquet. Id porta nibh venenatis cras sed felis eget. Pulvinar sapien et ligula ullamcorper. Sit amet consectetur adipiscing elit duis tristique. Tortor aliquam nulla facilisi cras. Et netus et malesuada fames ac turpis egestas. Quis auctor elit sed vulputate mi. Tincidunt nunc pulvinar sapien et. Iaculis nunc sed augue lacus viverra vitae congue eu consequat."""
     ipsum2 = "Est ante in nibh mauris. Adipiscing elit pellentesque habitant morbi tristique senectus et netus et. Volutpat diam ut venenatis tellus in metus vulputate eu. A diam sollicitudin tempor id eu nisl nunc mi. Vitae sapien pellentesque habitant morbi tristique senectus et. Adipiscing diam donec adipiscing tristique risus nec feugiat. Vestibulum mattis ullamcorper velit sed ullamcorper morbi tincidunt. Tellus integer feugiat scelerisque varius morbi enim nunc faucibus. Sit amet nisl suscipit adipiscing. Aliquam etiam erat velit scelerisque. Semper risus in hendrerit gravida rutrum quisque non tellus. Amet est placerat in egestas erat imperdiet sed. Porta non pulvinar neque laoreet suspendisse interdum consectetur. At consectetur lorem donec massa sapien."
     
-    return render_template('results.html', p1=english_to_babytalk(ipsum), p2=english_to_babytalk(ipsum2))
+    return render_template('results.html', p1=english_to_babytalk(ipsum), p2=english_to_babytalk(ipsum2), babypage = True)
 
 # route for form submission newtranslation
 @app.route('/newtranslation', methods=['POST'])
